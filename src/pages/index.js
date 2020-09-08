13509ca1b9327181d324e4dbb59689d0835c5523
Akshat Mittal <itsreallyakshat@gmail.com>
2020-09-09 05:27:26 +0530

    Update Head

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,15 +18,50 @@ export default function Home() {
         <CssBaseline />
         <Head>
           <title>Twitter Hashflag Browser</title>
+          <link rel="canonical" href="https://akshatmittal.com/twitter-hashflag-browser/" />
+
+          <link rel="shortcut icon" href="https://akshatmittal.com/twitter-hashflag-browser/favicon.png" />
+          <link
+            rel="icon"
+            type="image/png"
+            href="https://akshatmittal.com/twitter-hashflag-browser/favicon.png"
+            sizes="192x192"
+          />
+          <link rel="apple-touch-icon" href="https://akshatmittal.com/twitter-hashflag-browser/favicon.png" />
+          <link rel="android-touch-icon" href="https://akshatmittal.com/twitter-hashflag-browser/favicon.png" />
+          <link rel="windows-touch-icon" href="https://akshatmittal.com/twitter-hashflag-browser/favicon.png" />
+
+          <meta name="title" content="Twitter Hashflag Browser" />
           <meta
             name="description"
             content="Browse all current Twitter hashflags in a simple and easy to use interface!"
           />
-          <meta name="robots" content="index, follow" />
-          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-          <meta name="author" content="Akshat Mittal" />
-          <link rel="icon" href="favicon.png" />
-          <link rel="shortcut icon" href="favicon.png" />
+          <meta
+            name="keywords"
+            content="twitter, twitter hashflag, hashflag, hashflag browser, hashflags, hashflags browser, twitter hashflags browser"
+          />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://akshatmittal.com/twitter-hashflag-browser/" />
+          <meta property="og:title" content="Twitter Hashflag Browser" />
+          <meta
+            property="og:description"
+            content="Browse all current Twitter hashflags in a simple and easy to use interface!"
+          />
+          <meta property="og:image" content="https://akshatmittal.com/twitter-hashflag-browser/social.png" />
+          <meta property="og:site_name" content="Akshat Mittal" />
+          <meta property="fb:admins" content="100000581280466" />
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://akshatmittal.com/twitter-hashflag-browser/" />
+          <meta property="twitter:title" content="Twitter Hashflag Browser" />
+          <meta
+            property="twitter:description"
+            content="Browse all current Twitter hashflags in a simple and easy to use interface!"
+          />
+          <meta property="twitter:image" content="https://akshatmittal.com/twitter-hashflag-browser/social.png" />
+          <meta property="twitter:site" content="@iakshatmittal" />
+          <meta property="twitter:creator" content="@iakshatmittal" />
         </Head>
         <Menu toggleDarkMode={toggleDarkMode} />
         <Header />
